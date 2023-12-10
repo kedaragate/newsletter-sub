15eedf57a29ec66c51e6bc2132709f1e730bbf20
Kedar Agate <kedar.agate@gmail.com>
2023-12-10 10:33:20 +0530

    desktop version completed

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 let container = document.querySelector(".container");
 let subscriptionDiv = document.querySelector(".thanks-div");
 let emailId = document.querySelector("#email-id");
-let input = document.querySelector("#input-email");
+let userEmailId = document.querySelector("#input-email");
 let subscribeBtn = document.getElementById("subscribe-btn");
 let dismissBtn = document.getElementById("dismiss-btn");
+let validEmailRequiredText = document.querySelector(".validEmailRequired");
 
-checkForCorrectInput(input);
 function checkForCorrectInput(input) {
-  console.log(input.getAttribute("type"));
-  if (input.getAttribute("type") === "email") {
-    subscribeBtn.addEventListener("click", (e) => {
-      container.classList.toggle("hidden");
-      subscriptionDiv.classList.toggle("hidden");
-    });
+  let validEmailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (input.match(validEmailRegex)) {
+    return true;
   } else {
-    console.log("valid email required");
+    return false;
   }
 }
+subscribeBtn.addEventListener("click", () => {
+  if (checkForCorrectInput(userEmailId.value)) {
+    container.classList.toggle("hidden");
+    subscriptionDiv.classList.toggle("hidden");
+    emailId.textContent = userEmailId.value;
+  } else {
+    userEmailId.style.backgroundColor = "hsla(4, 100%, 67%,0.3)";
+    userEmailId.style.border = "0.5px solid hsl(4, 100%, 67%)";
+    userEmailId.style.color = "hsl(4, 100%, 67%)";
+    validEmailRequiredText.classList.toggle("hidden");
+  }
+});
 
 function dismiss() {
   container.classList.toggle("hidden");
   subscriptionDiv.classList.toggle("hidden");
+}
+
+function onFocus(ele) {
+  ele.style.backgroundColor = "hsl(0, 0%, 100%)";
+  ele.style.border = "0.5px solid hsl(234, 29%, 20%)";
+  userEmailId.style.color = "hsl(235, 18%, 26%)";
+  validEmailRequiredText.classList.add("hidden");
 }
